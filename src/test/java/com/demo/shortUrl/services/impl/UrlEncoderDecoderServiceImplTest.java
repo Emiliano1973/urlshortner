99a062865fbcf87f126beee951bd7c85c6f4d85c
emiliano.echinofora <emiliano.echinofora@gmail.com>
2024-02-12 21:28:22 +0000

    import optimized

--- src/test/java/com/demo/shortUrl/services/impl/UrlEncoderDecoderServiceImplTest.java
+++ src/test/java/com/demo/shortUrl/services/impl/UrlEncoderDecoderServiceImplTest.java
@@ -18,7 +18,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
